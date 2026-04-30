7d8bad6c468fbfc37fe4fef6dec43b07bb1062c6
daniyamz <terkimbiterungwa@gmail.com>
2026-04-30 07:39:48 +0100

    Main

--- main.go
+++ main.go
@@ -9,22 +9,53 @@ import (
 
 func main() {
 	var words []string
+	if len(os.Args) < 2 {
+		fmt.Println("Argument can't be less then 2. Usage go run . <string>")
+		return
+	}
+	if len(os.Args) > 2 {
+		fmt.Println("Too many arguments, wrap your input in quotes")
+		return
+	}
+
 	inputfile := os.Args[1]
+	if inputfile == "" {
+		return
+	}
+	// To check for space in the input
+	new_inputfile := strings.Split(inputfile, " ")
+	isspace := true
+	for _, val := range new_inputfile {
+		if val != "" {
+			isspace = false
+		}
+	}
+	if isspace {
+		return
+	}
+
+	inputfile = strings.TrimSpace(inputfile)
+	inputfile = strings.ReplaceAll(inputfile, "\\n", "\n")
 
 	new_text := process.AsciiArt("ascii/standard.txt")
-	//fmt.Println(new_text)
-	splitinputfile := strings.Split(inputfile, "/n")
+
+	splitinputfile := strings.Split(inputfile, "\n")
 	var start int
 	for _, word := range splitinputfile {
 		if word == "" {
 			fmt.Println()
+			return
 		}
-		for i := 0; i < 8; i++ {
+		for i := range 8 {
 			for _, ch := range word {
 				if ch < 32 || ch > 126 {
-					ch = ' '
+					fmt.Println(string(ch))
+
+					return
+					//ch = ' '
+
 				}
-				start = (int(ch)-32)*9 + 1
+				start = (int(ch)-32)*9 + 1 // the starting point of you the character.
 				words = new_text[start : start+8]
 				fmt.Print(words[i])
 			}
@@ -32,58 +63,3 @@ func main() {
 		}
 	}
 }
-
-// if len(os.Args) < 2 {
-// 	fmt.Println("Argument can't be less then 2. Usage go run . <string>")
-// 	return
-// }
-// if len(os.Args) > 2 {
-// 	fmt.Println("Too many arguments, wrap your input in quotes")
-// 	return
-// }
-// input := os.Args[1]
-// if input == "" { // handle empty input
-// 	return
-// }
-// // checking if the input is only a space and if there are space before a word.
-// spacesplite := strings.Split(input, " ")
-// space := true
-
-// for _, x := range spacesplite {
-// 	if x != "" {
-// 		space = false
-// 	}
-// }
-// if space {
-// 	return
-// }
-// input = strings.TrimSpace(input) // trim space before you continue with the input.
-// input = strings.ReplaceAll(input, "\\n", "\n")
-// var text []string
-// cont, err := os.ReadFile("ascii/standard.txt")
-// if err != nil {
-// 	fmt.Println("Error occured", err)
-// 	return
-// }
-// splitcont := strings.Split(string(cont), "\n")
-// inputtxt := strings.Split(input, "\n")
-
-// var start int
-// for _, cha := range inputtxt {
-// 	if cha == "" {
-// 		fmt.Println()
-// 	}
-// 	for i := 0; i < 8; i++ {
-// 		for _, words := range cha {
-// 			if words < 32 || words > 126 {
-// 				words = ' '
-// 			}
-// 			start = (int(words)-32)*9 + 1 // the beginning of the characters
-// 			text = splitcont[start : start+8]
-// 			fmt.Print(text[i])
-// 		}
-
-// 		fmt.Println()
-// 	}
-// }
-// process.AsciiArt()
