83f8a2fa773ff5ba3297db1743984845a93b2a61
daniyamz <terkimbiterungwa@gmail.com>
2026-04-30 05:04:43 +0100

    new

--- ascii/banner.go
+++ ascii/banner.go
@@ -1,64 +1,31 @@
 package process
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"strings"
 )
 
-func AsciiArt(filename string) {
-	if len(os.Args) < 2 {
-		fmt.Println("Argument can't be less then 2. Usage go run . <string>")
-		return
-	}
-	if len(os.Args) > 2 {
-		fmt.Println("Too many arguments, wrap your input in quotes")
-		return
-	}
-	input := os.Args[1]
-	if input == "" { // handle empty input
-		return
-	}
-	// checking if the input is only a space and if there are space before a word.
-	spacesplite := strings.Split(input, " ")
-	space := true
-
-	for _, x := range spacesplite {
-		if x != "" {
-			space = false
-		}
-	}
-	if space {
-		return
-	}
-	input = strings.TrimSpace(input) // trim space before you continue with the input.
-	input = strings.ReplaceAll(input, "\\n", "\n")
+func AsciiArt(filename string) []string {
 	var text []string
-	cont, err := os.ReadFile("ascii/standard.txt")
+
+	file, err := os.Open(filename)
 	if err != nil {
-		fmt.Println("Error occured", err)
-		return
+		log.Fatal()
 	}
-	splitcont := strings.Split(string(cont), "\n")
-	inputtxt := strings.Split(input, "\n")
-
-	var start int
-	for _, cha := range inputtxt {
-		if cha == "" {
-			fmt.Println()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		splittext := strings.Split(scanner.Text(), "\n")
+		for x := range splittext {
+			text = append(text, splittext[x])
 		}
-		for i := 0; i < 8; i++ {
-			for _, words := range cha {
-				if words < 32 || words > 126 {
-					words = ' '
-				}
-				start = (int(words)-32)*9 + 1 // the beginning of the characters
-				text = splitcont[start : start+8]
-				fmt.Print(text[i])
-			}
-
-			fmt.Println()
-		}
-
 	}
+	err = scanner.Err()
+	if err != nil {
+		fmt.Print("Error", err)
+	}
+	file.Close()
+	return text
 }
--- main.go
+++ main.go
@@ -1,7 +1,89 @@
 package main
 
-import process "ascii-art/ascii"
+import (
+	process "ascii-art/ascii"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	process.AsciiArt()
+	var words []string
+	inputfile := os.Args[1]
+
+	new_text := process.AsciiArt("ascii/standard.txt")
+	//fmt.Println(new_text)
+	splitinputfile := strings.Split(inputfile, "/n")
+	var start int
+	for _, word := range splitinputfile {
+		if word == "" {
+			fmt.Println()
+		}
+		for i := 0; i < 8; i++ {
+			for _, ch := range word {
+				if ch < 32 || ch > 126 {
+					ch = ' '
+				}
+				start = (int(ch)-32)*9 + 1
+				words = new_text[start : start+8]
+				fmt.Print(words[i])
+			}
+			fmt.Println()
+		}
+	}
 }
+
+// if len(os.Args) < 2 {
+// 	fmt.Println("Argument can't be less then 2. Usage go run . <string>")
+// 	return
+// }
+// if len(os.Args) > 2 {
+// 	fmt.Println("Too many arguments, wrap your input in quotes")
+// 	return
+// }
+// input := os.Args[1]
+// if input == "" { // handle empty input
+// 	return
+// }
+// // checking if the input is only a space and if there are space before a word.
+// spacesplite := strings.Split(input, " ")
+// space := true
+
+// for _, x := range spacesplite {
+// 	if x != "" {
+// 		space = false
+// 	}
+// }
+// if space {
+// 	return
+// }
+// input = strings.TrimSpace(input) // trim space before you continue with the input.
+// input = strings.ReplaceAll(input, "\\n", "\n")
+// var text []string
+// cont, err := os.ReadFile("ascii/standard.txt")
+// if err != nil {
+// 	fmt.Println("Error occured", err)
+// 	return
+// }
+// splitcont := strings.Split(string(cont), "\n")
+// inputtxt := strings.Split(input, "\n")
+
+// var start int
+// for _, cha := range inputtxt {
+// 	if cha == "" {
+// 		fmt.Println()
+// 	}
+// 	for i := 0; i < 8; i++ {
+// 		for _, words := range cha {
+// 			if words < 32 || words > 126 {
+// 				words = ' '
+// 			}
+// 			start = (int(words)-32)*9 + 1 // the beginning of the characters
+// 			text = splitcont[start : start+8]
+// 			fmt.Print(text[i])
+// 		}
+
+// 		fmt.Println()
+// 	}
+// }
+// process.AsciiArt()
